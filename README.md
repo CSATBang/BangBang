9dd1c92920fb7a970a4441270b3921c8ff12e03a
念 <3209752489@qq.com>
2022-04-20 19:07:41 +0800

    使用Vue3.0框架

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# BangBang
-模拟仿真试验
+# bang
+```
+精馏塔模拟实验
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <transition
+        mode="out-in"
+        enter-active-class="animate__animated animate__fadeIn"
+        leave-active-class="animate__animated animate__fadeOut">
+        <router-view v-slot="{Component}">
+            <keep-alive>
+                <component :is="Component"/>
+            </keep-alive>
+        </router-view>
+    </transition>
+</template>
+
+<script setup>
+let route = require('vue-router').useRoute();
+let currentRouter = route.path;
+</script>
