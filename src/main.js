e7060dd512cb6e2943fe086d07c6920ebbc0e33b
念 <78131932+wonder-light@users.noreply.github.com>
2022-04-21 16:46:04 +0800

    Revert "大致的样板，"数据处理"除外"

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
-import './assets/global.scss';
-import 'animate.css';
+import './assets/global.scss'
+import 'animate.css'
 
-createApp(App)
-    .use(store)
-    .use(router)
-    .mount('#app');
+createApp(App).use(store).use(router).mount('#app');
