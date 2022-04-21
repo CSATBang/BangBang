a4f66ed9c40d6b7f8f6648b4644df852b6789c5b
念 <3209752489@qq.com>
2022-04-21 16:39:00 +0800

    大致的样板，"数据处理"除外
    Signed-off-by: 念 <3209752489@qq.com>

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
-import './assets/global.scss'
-import 'animate.css'
+import './assets/global.scss';
+import 'animate.css';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app');
